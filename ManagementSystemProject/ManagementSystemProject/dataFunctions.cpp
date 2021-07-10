06f47337cf935ec7c7cd178ea11f74926b56bcae
RVVelichkova18 <RVVelichkova18@codingburgas.bg>
2021-07-10 10:49:58 +0300

    Add all get functions

--- ManagementSystemProject/ManagementSystemProject/dataFunctions.cpp
+++ ManagementSystemProject/ManagementSystemProject/dataFunctions.cpp
@@ -7,7 +7,7 @@
 #include "dataStructures.h"
 
 using namespace std;
-
+//functions for geting input
 string cinLine()
 {
 	//cin.ignore(1, '\n');
@@ -15,7 +15,6 @@ string cinLine()
 	getline(cin, line);
 	return line;
 }
-
 int cinNumber()
 {
 	//cin.ignore(1, '\n');
@@ -24,6 +23,8 @@ int cinNumber()
 	return stoi(line);
 }
 
+
+//functions managing users
 void USER::displayUsers()
 {
 	cout << "User id: " << this->id << endl;
@@ -36,60 +37,6 @@ void USER::displayUsers()
 	cout << "Date of last change: " << this->dateOfLastChange << endl;
 	cout << "Id of the last changer: " << this->idLastChange << endl;
 }
-void TEAMS::displayTeams()
-{
-	cout << "Team id: " << this->id << endl;
-	cout << "Title: " << this->title << endl;
-	cout << "Date of creation: " << this->dateOfCreation << endl;
-	cout << "Id of the creator: " << this->idCreator << endl;
-	cout << "Id of the last changer: " << this->idLastChange << endl;
-}
-void PROJECTS::displayProjects()
-{
-	cout << "Project id: " << this->id << endl;
-	cout << "Title: " << this->title << endl;
-	cout << "Description: " << this->description << endl;
-	cout << "Date of creation: " << this->dateOfCreation << endl;
-	cout << "Id of the creator: " << this->idCreator << endl;
-	cout << "Id of the last changer: " << this->idLastChange << endl;
-}
-
-string getTaskStatusName(TASK_STATUS& ts)
-{
-	switch (ts)
-	{
-	case TASK_STATUS::PENDING: return "pending";
-	case TASK_STATUS::IN_PROGRESS: return "in progress";
-	case TASK_STATUS::COMPLETED: return "completed";
-	};
-}
-
-void TASKS::displayTasks()
-{
-	cout << "Task id: " << this->id << endl;
-	cout << "Project id: " << this->projectId << endl;
-	cout << "User id: " << this->userId << endl;
-	cout << "Title: " << this->title << endl;
-	cout << "Description: " << this->description << endl;
-	cout << "Status: " << getTaskStatusName(this->status) << endl;
-	cout << "Date of creation: " << this->dateOfCreation << endl;
-	cout << "Id of the creator: " << this->idCreator << endl;
-	cout << "Id of the last changer: " << this->idLastChange << endl;
-
-	// task.status = (TASK_STATUS)result.get<int>("Status")
-
-}
-
-void LOGS::displayLogs() {
-	cout << "Log id: " << this->id << endl;
-	cout << "Task id: " << this->taskId << endl;
-	cout << "User id: " << this->userId << endl;
-	cout << "Time: " << this->time << endl;
-	cout << "Date: " << this->date << endl;
-}
-
-
-
 void createUser(nanodbc::connection conn)
 {
 	nanodbc::statement statement(conn);
@@ -122,8 +69,6 @@ void createUser(nanodbc::connection conn)
 
 	execute(statement);
 }
-
-
 void editUser(nanodbc::connection conn)
 {
 
@@ -167,7 +112,6 @@ void editUser(nanodbc::connection conn)
 
 	execute(statement);
 }
-
 vector<USER> getAllUsers(nanodbc::connection conn)
 {
 	vector<USER> users;
@@ -189,9 +133,9 @@ vector<USER> getAllUsers(nanodbc::connection conn)
 		user.firstName = result.get<nanodbc::string>("FirstName", "");
 		user.lastName = result.get<nanodbc::string>("LastName", "");
 		user.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
-		user.idCreator = result.get<int>("Id",0);
+		user.idCreator = result.get<int>("idCreator",0);
 		user.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
-		user.idLastChange = result.get<int>("Id",0);
+		user.idLastChange = result.get<int>("idLastChange",0);
 		user.isAdmin = result.get<int>("isAdmin");
 		
 
@@ -199,4 +143,440 @@ vector<USER> getAllUsers(nanodbc::connection conn)
 	}
 
 	return users;
+}
+
+
+//functions managing teams
+void TEAMS::displayTeams()
+{
+	cout << "Team id: " << this->id << endl;
+	cout << "Title: " << this->title << endl;
+	cout << "Date of creation: " << this->dateOfCreation << endl;
+	cout << "Id of the creator: " << this->idCreator << endl;
+	cout << "Id of the last changer: " << this->idLastChange << endl;
+}
+void createTeam(nanodbc::connection conn)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+           [ManagementSystemProject].[dbo].[Teams]
+            (Title, DateOfCreation, IdCreator, DateLastChange, IdLastCHange)
+            VALUES
+            ( ?, GETDATE(), ?, GETDATE(), ?)
+    )"));
+
+	cout << "Enter team's name: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter the id of the creator: ";
+	const int idCreator = cinNumber();
+	statement.bind(1, &idCreator);
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(2, &idLastChange);
+
+	execute(statement);
+}
+void editTeam(nanodbc::connection conn)
+{
+
+	nanodbc::statement statement(conn);
+
+	cout << "Enter id of the team that you want edit: " << endl;
+	const int id = cinNumber();
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+      UPDATE [ManagementSystemProject].[dbo].[Teams]
+	  SET
+			Title = ?
+			,DateOfLastChange = GETDATE()
+			,IdLastChange = ?
+	  WHERE Id=?;
+    )"));
+
+	cout << "Enter the team's new name: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(1, &idLastChange);
+
+	statement.bind(2, &id);
+
+	execute(statement);
+}
+vector<TEAMS> getAllTeams(nanodbc::connection conn)
+{
+	vector<TEAMS> teams;
+
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM [ManagementSystemProject].[dbo].[Teams]
+    )"));
+
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		TEAMS team;
+		team.id = result.get<int>("Id");
+		team.title = result.get<nanodbc::string>("Title", "");
+		team.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		team.idCreator = result.get<int>("idCreator", 0);
+		team.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		team.idLastChange = result.get<int>("idLastChange", 0);
+
+		teams.push_back(team);
+	}
+
+	return teams;
+}
+
+//functions managing projects
+void PROJECTS::displayProjects()
+{
+	cout << "Project id: " << this->id << endl;
+	cout << "Title: " << this->title << endl;
+	cout << "Description: " << this->description << endl;
+	cout << "Date of creation: " << this->dateOfCreation << endl;
+	cout << "Id of the creator: " << this->idCreator << endl;
+	cout << "Id of the last changer: " << this->idLastChange << endl;
+}
+void createProject(nanodbc::connection conn)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+           [ManagementSystemProject].[dbo].[Projects]
+            (Title, Description, DateOfCreation, IdCreator, DateLastChange, IdLastCHange)
+            VALUES
+            ( ?, ?, GETDATE(), ?, GETDATE(), ?)
+    )"));
+
+	cout << "Enter project's name: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter project's description: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter the id of the creator: ";
+	const int idCreator = cinNumber();
+	statement.bind(1, &idCreator);
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(2, &idLastChange);
+
+	execute(statement);
+}
+void editProject(nanodbc::connection conn)
+{
+
+	nanodbc::statement statement(conn);
+
+	cout << "Enter id of the project that you want edit: " << endl;
+	const int id = cinNumber();
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+      UPDATE [ManagementSystemProject].[dbo].[Projects]
+	  SET
+			Title = ?
+			,Description = ?
+			,DateOfLastChange = GETDATE()
+			,IdLastChange = ?
+	  WHERE Id=?;
+    )"));
+
+	cout << "Enter the title: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter description: ";
+	const string description = cinLine();
+	statement.bind(1, description.c_str());
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(2, &idLastChange);
+
+	statement.bind(3, &id);
+
+	execute(statement);
+}
+vector<PROJECTS> getAllProjects(nanodbc::connection conn)
+{
+	vector<PROJECTS> projects;
+
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM [ManagementSystemProject].[dbo].[Projects]
+    )"));
+
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		PROJECTS project;
+		project.id = result.get<int>("Id");
+		project.title = result.get<nanodbc::string>("Title", "");
+		project.description = result.get<nanodbc::string>("Description", "");
+		project.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		project.idCreator = result.get<int>("idCreator", 0);
+		project.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		project.idLastChange = result.get<int>("idLastChange", 0);
+
+		projects.push_back(project);
+	}
+
+	return projects;
+}
+
+//functions managing tasks
+string getTaskStatusName(TASK_STATUS& ts)
+{
+	switch (ts)
+	{
+	case TASK_STATUS::PENDING: return "pending";
+	case TASK_STATUS::IN_PROGRESS: return "in progress";
+	case TASK_STATUS::COMPLETED: return "completed";
+	};
+}
+void TASKS::displayTasks()
+{
+	cout << "Task id: " << this->id << endl;
+	cout << "Project id: " << this->projectId << endl;
+	cout << "User id: " << this->userId << endl;
+	cout << "Title: " << this->title << endl;
+	cout << "Description: " << this->description << endl;
+	cout << "Status: " << getTaskStatusName(this->status) << endl;
+	cout << "Date of creation: " << this->dateOfCreation << endl;
+	cout << "Id of the creator: " << this->idCreator << endl;
+	cout << "Id of the last changer: " << this->idLastChange << endl;
+
+	// task.status = (TASK_STATUS)result.get<int>("Status")
+
+}
+void createTask(nanodbc::connection conn)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+           [ManagementSystemProject].[dbo].[Tasks]
+            (ProjectId, UserId, Title, Description, DateOfCreation, IdCreator, DateLastChange, IdLastCHange)
+            VALUES
+            ( ?, ?, ?, ?, GETDATE(), ?, GETDATE(), ?)
+    )"));
+
+	cout << "Enter project id: ";
+	const int projectId = cinNumber();
+	statement.bind(0, &projectId);
+
+	cout << "Enter assignee id: ";
+	const int userId = cinNumber();
+	statement.bind(1, &userId);
+
+	cout << "Enter task's name: ";
+	const string title = cinLine();
+	statement.bind(2, title.c_str());
+
+	cout << "Enter task's description: ";
+	const string description = cinLine();
+	statement.bind(3, description.c_str());
+
+	cout << "Enter task's status: ";
+	const string status = cinLine();
+	statement.bind(4, status.c_str());
+
+	cout << "Enter the id of the creator: ";
+	const int idCreator = cinNumber();
+	statement.bind(5, &idCreator);
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(6, &idLastChange);
+
+	execute(statement);
+}
+void editTask(nanodbc::connection conn)
+{
+
+	nanodbc::statement statement(conn);
+
+	cout << "Enter id of the task that you want edit: " << endl;
+	const int id = cinNumber();
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+      UPDATE [ManagementSystemProject].[dbo].[Projects]
+	  SET
+			UserId = ?
+			,Title = ?
+			,Description = ?
+			,Status = ?
+			,DateOfLastChange = GETDATE()
+			,IdLastChange = ?
+	  WHERE Id=?;
+    )"));
+
+	cout << "Enter the id of the new assignee: ";
+	const string userId = cinLine();
+	statement.bind(0, userId.c_str());
+
+	cout << "Enter the title: ";
+	const string title = cinLine();
+	statement.bind(0, title.c_str());
+
+	cout << "Enter description: ";
+	const string description = cinLine();
+	statement.bind(1, description.c_str());
+
+	cout << "Enter new status: ";
+	const string status = cinLine();
+	statement.bind(1, status.c_str());
+
+
+	cout << "Enter the id of the person that did the last change: ";
+	const int idLastChange = cinNumber();
+	statement.bind(2, &idLastChange);
+
+	statement.bind(3, &id);
+
+	execute(statement);
+}
+vector<TASKS> getAllTasks(nanodbc::connection conn)
+{
+	vector<TASKS> tasks;
+
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM [ManagementSystemProject].[dbo].[Tasks]
+    )"));
+
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		TASKS task;
+		task.id = result.get<int>("Id");
+		task.projectId = result.get<int>("ProjectId");
+		task.userId = result.get<int>("UserId");
+		task.title = result.get<nanodbc::string>("Title", "");
+		task.description = result.get<nanodbc::string>("Description", "");
+		task.status = (TASK_STATUS)result.get<int>("Status");
+		task.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		task.idCreator = result.get<int>("idCreator", 0);
+		task.dateOfCreation = result.get<nanodbc::string>("DateOfCreation", "");
+		task.idLastChange = result.get<int>("idLastChange", 0);
+
+		tasks.push_back(task);
+	}
+
+	return tasks;
+}
+
+
+//functions managing work logs
+void LOGS::displayLogs() {
+	cout << "Log id: " << this->id << endl;
+	cout << "Task id: " << this->taskId << endl;
+	cout << "User id: " << this->userId << endl;
+	cout << "Time: " << this->time << endl;
+	cout << "Date: " << this->date << endl;
+}
+void createLog(nanodbc::connection conn)
+{
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+           [ManagementSystemProject].[dbo].[WorkLog]
+            (TaskId, UserId, Time, Date)
+            VALUES
+            ( ?, ?, ?, ?)
+    )"));
+
+	cout << "Enter task id: ";
+	const int taskId = cinNumber();
+	statement.bind(0, &taskId);
+
+	cout << "Enter assignee id: ";
+	const int userId = cinNumber();
+	statement.bind(1, &userId);
+
+	cout << "Enter working hours: ";
+	const int time = cinNumber();
+	statement.bind(2, &time);
+
+	cout << "Enter the date when you are going to work: ";
+	const int date = cinNumber();
+	statement.bind(3, &date);
+
+	execute(statement);
+}
+void editLog(nanodbc::connection conn)
+{
+
+	nanodbc::statement statement(conn);
+
+	cout << "Enter id of the work log that you want edit: " << endl;
+	const int id = cinNumber();
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+      UPDATE [ManagementSystemProject].[dbo].[WorkLog]
+	  SET
+			TaskId = ?
+			,UserId = ?
+			,Time = ?
+			,Date = ?
+	  WHERE Id=?;
+    )"));
+
+	cout << "Enter the id of the new task: ";
+	const string taskId = cinLine();
+	statement.bind(0, taskId.c_str());
+
+	cout << "Enter the id of the new assignee:: ";
+	const string userId = cinLine();
+	statement.bind(1, userId.c_str());
+
+	cout << "Enter working hours: ";
+	const string time = cinLine();
+	statement.bind(2, time.c_str());
+
+	cout << "Enter  the date when you are going to work: ";
+	const string date = cinLine();
+	statement.bind(3, date.c_str());
+
+	statement.bind(4, &id);
+
+	execute(statement);
+}
+vector<LOGS> getAllLogs(nanodbc::connection conn)
+{
+	vector<LOGS> logs;
+
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM [ManagementSystemProject].[dbo].[Projects]
+    )"));
+
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		LOGS log;
+		log.id = result.get<int>("Id");
+		log.taskId = result.get<int>("TaskId");
+		log.userId = result.get<int>("UserId");
+		log.time = result.get<int>("Time");
+		log.date = result.get<nanodbc::string>("Date", "");
+
+		logs.push_back(log);
+	}
+
+	return logs;
 }
